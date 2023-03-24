576e6b59c64da8322ed7c975962cb58681262bcc
Pallab Debnath <63156810+PallabDN@users.noreply.github.com>
2023-03-24 13:36:48 +0530

    ML model

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
-#model = pickle.load(open('iri.pkl', 'rb'))
+model = pickle.load(open('iri.pkl', 'rb'))
 
 app = Flask(__name__)
 
@@ -13,19 +13,21 @@ def man():
     return render_template('index.html')
 
 
-#@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 
-"""
 def home():
     data1 = request.form['a']
     data2 = request.form['b']
     data3 = request.form['c']
     data4 = request.form['d']
-    arr = np.array([[data1, data2, data3, data4]])
+    data5 = request.form['d']
+    data6 = request.form['d']
+    data7 = request.form['d']
+    data8 = request.form['d']
+    arr = np.array([[data1, data2, data3, data4, data5, data6, data7, data8]])
     pred = model.predict(arr)
     return render_template('after.html', data=pred)
 
-"""
 
 
 if __name__ == "__main__":
